90a8752aa3bfd4b61541d57144bda4dd72bcd898
Caleb <choover11@gmail.com>
2021-12-28 11:30:30 -0800

    updated bin/updatePackages.sh to print out what the script is doing and also update home-manager

--- bin/updatePackages.sh
+++ bin/updatePackages.sh
@@ -1,3 +1,11 @@
+echo "Updating flake.lock nixpkgs"
 nix flake lock --update-input nixpkgs /home/caleb/.config/nix
-nixos-rebuild switch --flake /home/caleb/.config/nix
+echo "Updating flake.lock home-manager"
+nix flake lock --update-input home-manager /home/caleb/.config/nix
+
+read -p "rebuild nixos? " -i y option
+
+if [[ $option == "y" || $option == "Y" ]]; then
+  nixos-rebuild switch --flake /home/caleb/.config/nix
+fi
 
